8359a2702965f94bfc4ace9b79eacfca956be44a
saifmathur <saifmathur41@gmail.com>
2025-01-21 16:42:01 +0530

    initial commit

---
+++ src/test/java/com/journal/journal_service/JournalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journal.journal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
