92238c3c5c17d8d2307220a7d9926dc2443bf6a3
saifmathur <saifmathur41@gmail.com>
2025-01-23 18:14:02 +0530

    added vercel app in CORS

--- src/main/java/com/journal/journal_service/config/WebConfig.java
+++ src/main/java/com/journal/journal_service/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow CORS for all paths
-                .allowedOrigins("http://localhost:4200")  // Allow your Angular app origin
+                .allowedOrigins("http://localhost:4200","https://journal-f2w2wonph-saifmathurs-projects.vercel.app/")  // Allow your Angular app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow sending credentials (cookies, authorization headers)
