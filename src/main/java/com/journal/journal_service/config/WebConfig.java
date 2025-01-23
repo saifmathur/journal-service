4c5c7659fa89c5c7d9461786c53e0d05390945ce
saifmathur <saifmathur41@gmail.com>
2025-01-23 15:24:44 +0530

    CORS resolved

---
+++ src/main/java/com/journal/journal_service/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.journal.journal_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Allow CORS for specific URL patterns
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Allow frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (cookies, etc.)
+    }
+}
