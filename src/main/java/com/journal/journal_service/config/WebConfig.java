3e8f2db831313034c4670d11c45e061cf3be416c
saifmathur <saifmathur41@gmail.com>
2025-01-25 10:58:01 +0530

    added vercel app id as env

--- src/main/java/com/journal/journal_service/config/WebConfig.java
+++ src/main/java/com/journal/journal_service/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.journal.journal_service.config;
 
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow CORS for all paths
-                .allowedOrigins("http://localhost:4200","https://*saifmathurs-projects.vercel.app")  // Allow your Angular app origin
+                .allowedOrigins("http://localhost:4200","https://journal-"+System.getenv("VERCEL_APP_ID")+"-saifmathurs-projects.vercel.app/")  // Allow your Angular app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true);  // Allow sending credentials (cookies, authorization headers)
