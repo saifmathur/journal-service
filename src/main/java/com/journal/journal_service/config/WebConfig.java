8e6853896b446f2174a9c55eb41e659a618c71aa
saifmathur <saifmathur41@gmail.com>
2025-01-27 17:41:56 +0530

    cors prod

--- src/main/java/com/journal/journal_service/config/WebConfig.java
+++ src/main/java/com/journal/journal_service/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow CORS for all paths
-                .allowedOrigins("http://localhost:4200","https://journal-"+System.getenv("VERCEL_APP_ID")+"-saifmathurs-projects.vercel.app/")  // Allow your Angular app origin
+                .allowedOrigins("http://localhost:4200","https://journal-"+System.getenv("VERCEL_APP_ID")+"-saifmathurs-projects.vercel.app/","https://journal-taupe-theta.vercel.app")  // Allow your Angular app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("Authorization", "Content-Type")  // Allow all headers
                 .allowCredentials(true);  // Allow sending credentials (cookies, authorization headers)
